8042dccf45c95b466bc77f5d8372ef9ec156d623
simonehalstead <simone-halstead@hotmail.com>
2017-11-30 03:07:37 +1300

    edited app.js with id and pass

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: "3e299e72-d25f-4734-80c1-f525178655a4",
+    appPassword: "mSQPJ756|cqzjvhYJF55~(-"
 });
 
 // Listen for messages from users 
